b71c82dc9c88aaf8c4740f7456231edde800d372
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-05 09:20:37 -0400

    Attribute directives last steps.

--- directives/src/app/highlight.directive.ts
+++ directives/src/app/highlight.directive.ts
@@ -1,18 +1,18 @@
-import {Directive, ElementRef, Renderer, HostListener, HostBinding} from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, HostBinding, Input, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[highlight]' // this selector works just as a css selector does, we can use # or . to select using class or id attribute.
 })
-export class HighlightDirective {
-  private backgroundColor: string = 'white';
+export class HighlightDirective implements  OnInit{
+  private backgroundColor: string;
 
   @HostListener('mouseenter') mouseover() {
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave', ['$event']) mouseout(eventObject) { // I can also catch the event object
     console.log(eventObject.target);
-    this.backgroundColor = 'white';
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostBinding('style.backgroundColor') get setColor (): string {
@@ -21,10 +21,16 @@ export class HighlightDirective {
 
   //private renderer: Renderer; // you can create the local variable explicit or simply add private in the constructor.
 
+  @Input() defaultColor: string = 'white';
+  @Input('highlight') highlightColor: string = 'green'; // Specifying this property name I won't have to add a different property binding to the host
+
   constructor() {
     // this.elementRef.nativeElement.style.backgroundColor = 'green';
     //  this.elementRef.nativeElement.style.backgroundColor =  'green';
+  }
 
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
   }
 
 }
