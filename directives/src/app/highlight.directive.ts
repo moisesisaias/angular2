65f3012c4a6692325e9c8bbd7af2ef85a26e9267
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-05 08:58:45 -0400

    Element Interaction with HostListenter and HostBinding

--- directives/src/app/highlight.directive.ts
+++ directives/src/app/highlight.directive.ts
@@ -1,14 +1,29 @@
-import {Directive, ElementRef, Renderer} from '@angular/core';
+import {Directive, ElementRef, Renderer, HostListener, HostBinding} from '@angular/core';
 
 @Directive({
   selector: '[highlight]' // this selector works just as a css selector does, we can use # or . to select using class or id attribute.
 })
 export class HighlightDirective {
-  private renderer: Renderer; // you can create the local variable explicit or simply add private in the constructor.
+  private backgroundColor: string = 'white';
 
-  constructor(private elementRef: ElementRef, renderer: Renderer) {
-    this.renderer = renderer;
-    this.elementRef.nativeElement.style.backgroundColor =  'green';
+  @HostListener('mouseenter') mouseover() {
+    this.backgroundColor = 'green';
+  }
+
+  @HostListener('mouseleave', ['$event']) mouseout(eventObject) { // I can also catch the event object
+    console.log(eventObject.target);
+    this.backgroundColor = 'white';
+  }
+
+  @HostBinding('style.backgroundColor') get setColor (): string {
+    return this.backgroundColor;
+  } // Instead of using @HostBinding I can simply use renderer in the @HostListener
+
+  //private renderer: Renderer; // you can create the local variable explicit or simply add private in the constructor.
+
+  constructor() {
+    // this.elementRef.nativeElement.style.backgroundColor = 'green';
+    //  this.elementRef.nativeElement.style.backgroundColor =  'green';
 
   }
 
