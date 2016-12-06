7729390767fd48b91a7f90f6140c9628cfdec4db
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-06 13:44:21 -0400

    Extracting Query Params

--- Routing - Start/src/app/home-component.component.ts
+++ Routing - Start/src/app/home-component.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {Subscription} from "rxjs";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-home-component',
   template: `
     <h1>
-      Home Component!
+      Home Component! 
     </h1>
+    <p *ngIf="!!param"> Query Param: {{param}}</p>
   `,
   styles: []
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy{
+  private subscription: Subscription;
 
+  param: string;
+
+  constructor(private route: ActivatedRoute) {
+    this.subscription = route.queryParams.subscribe(
+      (queryParam: any) => this.param = queryParam['analytics']
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
