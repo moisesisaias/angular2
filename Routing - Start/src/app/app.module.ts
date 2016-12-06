a7a74164723b0fad6aca71510f6c212ad4eba5de
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-06 16:02:44 -0400

    Guards: CanDeactivate

--- Routing - Start/src/app/app.module.ts
+++ Routing - Start/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { UserEditComponent } from "./user/user-edit.component";
 import { HomeComponent } from "./home-component.component";
 import {routing} from "./app.routing";
 import {UserDetailGuard} from "./user/user-detail.guard";
+import {UserEditGuard} from "./user/user-edit.guard";
 
 
 @NgModule({
@@ -20,6 +21,6 @@ import {UserDetailGuard} from "./user/user-detail.guard";
     ],
     imports: [BrowserModule, routing],
     bootstrap: [AppComponent],
-    providers: [UserDetailGuard]
+    providers: [UserDetailGuard, UserEditGuard]
 })
 export class AppModule {}
--- Routing - Start/src/app/user/user-edit.component.ts
+++ Routing - Start/src/app/user/user-edit.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {CanDeactivateComponent} from "./user-edit.guard";
+import {Observable} from "rxjs";
 
 @Component({
     selector: 'app-user-edit',
     template: `
         <h3>User Edit</h3>
+        <button (click)="done = true">Done</button>
+        <button class="btn btn-alert" (click)="onNavigate()">Go home</button>
     `
 })
-export class UserEditComponent {
+export class UserEditComponent implements CanDeactivateComponent{
+  private done: boolean = false;
+
+  constructor(private router: Router){
+
+  }
+
+  onNavigate(){
+    this.router.navigate(['/'],{queryParams:{'analytics': 100}, fragment: 'section1'});
+  }
+
+  canDeactivate(): Observable<boolean> | boolean {
+    if(!this.done){
+      return confirm('Do you want to leave?');
+    }
+    return true;
+  }
 }
