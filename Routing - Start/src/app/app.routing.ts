4d2545fd44224b37bf437294cbb253404fc6bca6
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-06 15:26:38 -0400

    routerLinkActive for styling the actual route and Guards to prevent access of unwanted users

--- Routing - Start/src/app/app.routing.ts
+++ Routing - Start/src/app/app.routing.ts
@@ -7,11 +7,10 @@ import {UserComponent} from "./user/user.component";
 import {USER_ROUTES} from "./user/user.routes";
 
 const APP_ROUTES: Routes = [
-  { path:'user', redirectTo: 'user/1', pathMatch: 'full' },
-  { path:'user/', redirectTo: 'user/1', pathMatch: 'full' },
   { path:'user/:id', component: UserComponent, children: USER_ROUTES },
   { path:'user/:id', component: UserComponent },
-  { path:'', component: HomeComponent }
-  ];
+  { path:'', component: HomeComponent },
+  { path:'**', redirectTo: 'user/1', pathMatch: 'full' }
+];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
