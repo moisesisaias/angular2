530db9eb4aa1592a51185bc88d0c7b68e21cf691
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-07 15:02:36 -0400

    ngSubmit

--- Forms - Start/src/app/template-driven/template-driven.component.ts
+++ Forms - Start/src/app/template-driven/template-driven.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {Form} from "@angular/forms";
 
 @Component({
   selector: 'template-driven',
   templateUrl: 'template-driven.component.html'
 })
 export class TemplateDrivenComponent {
+  user = {
+    username: '',
+    password: '',
+    email: ''
+  };
+  onSubmit(form: Form) {
+    console.log(form);
+  }
 }
