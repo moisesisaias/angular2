c2105124ff05576bdb63fb66feb3602bcc4b711c
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-08 16:22:15 -0400

    DataDrive: Form Group, Form Controls, Submit action, and input validation.

--- Forms - Start/src/app/data-driven/data-driven.component.ts
+++ Forms - Start/src/app/data-driven/data-driven.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
+import {FormGroup, FormControl, Validators} from "@angular/forms";
 
 @Component({
     selector: 'data-driven',
     templateUrl: 'data-driven.component.html'
 })
 export class DataDrivenComponent {
-    
+    myForm: FormGroup;
+
+    constructor(){
+      this.myForm = new FormGroup(
+        {
+          'username': new FormControl('Max', Validators.required),
+          'email': new FormControl('',
+            [
+              Validators.required,
+              Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            ]),
+          'password': new FormControl('', Validators.required)
+        }
+      );
+    }
+
+    onSubmit(){
+      console.log(this.myForm);
+    }
 }
