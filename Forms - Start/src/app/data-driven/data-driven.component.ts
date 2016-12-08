2dcd960af80fd60e12f4b670c994aaefcba5c182
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-08 16:51:38 -0400

    DataDrive: State States, and Form Groups

--- Forms - Start/src/app/data-driven/data-driven.component.ts
+++ Forms - Start/src/app/data-driven/data-driven.component.ts
@@ -11,12 +11,14 @@ export class DataDrivenComponent {
     constructor(){
       this.myForm = new FormGroup(
         {
-          'username': new FormControl('Max', Validators.required),
-          'email': new FormControl('',
-            [
-              Validators.required,
-              Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-            ]),
+          'userData': new FormGroup({
+            'username': new FormControl('Max', Validators.required),
+            'email': new FormControl('',
+              [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+              ])
+          }),
           'password': new FormControl('', Validators.required)
         }
       );
