92d652e0834a8a85791021382e3a05e165346f7e
Moisés Ramirez <moises.ramon.ramirez@gmail.com>
2016-12-09 11:12:34 -0400

    Forms: Implementing shopping list forms with template-driven approach.

--- recipe-book/src/app/shopping-list/shopping-list-add.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {ShoppingListService} from "./shopping-list.service";
+import {Ingredient} from "../ingredient";
 
 @Component({
   selector: 'rb-shopping-list-add',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ShoppingListAddComponent implements OnInit {
+  isAdd: boolean = true;
 
-  constructor() { }
+  constructor(private sls: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form);
+  }
+
+  onAdd(form: NgForm){
+    this.sls.addItems([new Ingredient(form.controls['name'].value, form.controls['amount'].value)]);
+    form.reset({'name':'', 'amount':''});
   }
 
 }
